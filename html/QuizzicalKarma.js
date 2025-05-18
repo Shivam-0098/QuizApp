e47146d5ab784c4d53de1938d1aae3d020c2e5ed
Shivam-0098 <162126602+Shivam-0098@users.noreply.github.com>
2025-05-18 13:19:14 +0530

    Add files via upload

---
+++ html/QuizzicalKarma.js
@@ -0,0 +1,483 @@
+/* Created and coded by Shivam Rawat */
+
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "What is the average of first five multiples of 12?",
+    "options": [
+    {
+    "a": "36;",
+    "b": "38;",
+    "c": "40;",
+    "d": "42;" 
+    }
+    ],
+    "answer": "36;",
+    "score": 0,
+    "status": "" 
+    },
+    {
+    "id": 2,
+    "question": "What is the difference in the place value of 5 in the numeral 754853?",
+    "options": [
+    {
+    "a": "49500",
+    "b": "49950",
+    "c": "45000",
+    "d": "49940"
+    }
+    ],
+    "answer": "49950",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "What is the compound interest on Rs.2500 for 2 years at rate of interest 4% per annum?",
+    "options": [
+    {
+    "a": "Rs.180;",
+    "b": "Rs.204;",
+    "c": "Rs.210;",
+    "d": "Rs.220"
+    }
+    ],
+    "answer": "204;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 4,
+    "question": "40% of 280 = ?.",
+    "options": [
+    {
+    "a": "112", 
+    "b": "116",
+    "c": "120",
+    "d": "125"
+    }
+    ],
+    "answer": "112",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "A running man crosses a bridge of length 500 meters in 4 minutes. At what speed he is running?",
+    "options": [
+    {
+    "a": "8.5 km/h;",
+    "b": "7.5 km/h;",
+    "c": "9.5 km/h;",
+    "d": "6.5 km/h;"
+    }
+    ],
+    "answer": "7.5 km/h",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 6,
+    "question": " Rohit from point ‘A’ walks 20m. to the east. Then turns right and walks 10m, again turns right and walks 9m, then turns left and walks 5m, again turns left and walks 12m, again turns left and walks 5m and reached at point B. In which direction is he going now?",
+    
+    "options": [
+    {
+    "a": "South",
+    "b": "North",
+    "c": "West",
+    "d": "East"
+    }
+    ],
+    "answer": "North",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 7,
+    "question": " Suresh is 7 ranks ahead of Ashok in the class of 39 students. If Ashok’s rank is 17th from the last. What is Suresh’s rank from the start?",
+    "options": [
+    {
+    "a": "16th",
+    "b": "23th",
+    "c": "24th",
+    "d": "15th"
+    }
+    ],
+    "answer": "16th",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 8,
+    "question": "Select the odd word from the given alternatives.?",
+    "options": [
+    {
+    "a": "206",
+    "b": "125",
+    "c": "27",
+    "d": " 8",
+    }
+    ],
+    "answer": "206",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 9,
+    "question": "A man facing towards North- West turns Left, Right, Right, Right, Left, in series. In which direction now he is?",
+    "options": [
+    {
+    "a": "North",
+    "b": "South-West",
+    "c": "South",
+    "d": "North-East"
+    }
+    ],
+    "answer": "North-East",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 10,
+    "question": "Select the odd word from the given alternatives.?",
+    "options": [
+    {
+    "a": "Heat",
+    "b": "Light",
+    "c": "Bulb",
+    "d": "Electricity",
+    }
+    ],
+    "answer": "Bulb",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 11,
+    "question": "Select the odd word from the given alternatives.?",
+    "options": [
+    {
+    "a": "Room",
+    "b": "Chamber",
+    "c": "Veranda",
+    "d": "Cabin"
+    }
+    ],
+    "answer": "Veranda",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 12,
+    "question": " Which is the biggest continent in the world?",
+    "options": [
+    {
+    "a": "North America",
+    "b": "Asia",
+    "c": "Africa",
+    "d": "Australia"
+    }
+    ],
+    "answer": "Asia",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 13,
+    "question": " Which is longest river in the world?",
+    "options": [
+    {
+    "a": "Great ganga",
+    "b": "Nile",
+    "c": "Amazon",
+    "d": "Niger"
+    }
+    ],
+    "answer": "Nile",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 14,
+    "question": "Which is the largest ocean in the world?",
+    "options": [
+    {
+    "a": "Indian Ocean",
+    "b": "Pacific Ocean",
+    "c": "Arctic Ocean",
+    "d": "Atlantic Ocean"
+    }
+    ],
+    "answer": "Pacific Ocean",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 15,
+     "question": "Which is india first super computer?",
+    "options": [
+    {
+    "a": "Param800 ",
+    "b": "Param80000",
+    "c": "Param800",
+    "d": "Param8"
+    }
+    ],
+    "answer": "Param8000",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 16,
+    "question": "Which is largest animal in the world?",
+    "options": [
+    {
+    "a": "sharke",
+    "b": "Blue Whale",
+    "c": "Elephant",
+    "d": "Giraffe"
+    }
+    ],
+    "answer": "Blue Whale",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 17,
+    "question": "Which continent is known as Dark Continent?",
+
+    
+    "options": [
+    {
+    "a": "Asia",
+    "b": "South America",
+    "c": "Australia",
+    "d": "Africa"
+    }
+    ],
+    "answer": "Africa",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 18,
+    "question": "Which is largest desert in the world?",
+    "options": [
+    {
+    "a": "Kalahari",
+    "b": "Gobi",
+    "c": "Sahara",
+    "d": "Taklamakan"
+    }
+    ],
+    "answer": "Sahara",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 19,
+    "question": "Which metal is the lightest metal in world?",
+    "options": [
+    {
+    "a": "Zinc",
+    "b": "Lithium",
+    "c": "Platinum",
+    "d": "gold"
+
+    }
+    ],
+    "answer": "Lithium",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 20,
+    "question": "I go to Madurai by ___ Vaigi Express.",
+    "options": [
+    {
+    "a": "a",
+    "b": "an",
+    "c": "some",
+    "d": "the"
+    }
+    ],
+    "answer": "the",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 21,
+    "question": "He hopes to join ___ university soon.",
+    "options": [
+    {
+    "a": "a",
+    "b": "an",
+    "c": "the", 
+    "d": "no article"
+    }
+    ],
+    "answer": "a",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 22,
+    "question": "Meaning of Tenacious",
+    "options": [
+    {
+    "a": "holding fast",
+    "b": "collecting",
+    "c": "fast running",
+    "d": "internal"
+    }
+    ],
+    "answer": "holding fast",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 23,
+    "question": "Select Correct Word",
+    "options": [
+    {
+    "a": "Ofence",
+    "b": "Ofance",
+    "c": "Offence",
+    "d": "Ofancee"
+    }
+    ],
+    "answer": "Offence",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 24,
+    "question": "Select Correct Word",
+    "options": [
+    {
+    "a": "Apreciation",
+    "b": "Appreciation",
+    "c": "Aprreciation",
+    "d": "Appereciation"
+    }
+    ],
+    "answer": "Appreciation",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 25,
+    "question": "Which of the following is mandatory for sending an email?",
+    "options": [
+    {
+    "a": "Body",
+    "b": "Sender Mail ID",
+    "c": "Attachment",
+    "d":"Subject"
+    }
+    ],
+    "answer": "Sender Mail ID",
+    "score": 0,
+    "status": ""
+    }
+    ]
+    }
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;  
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
